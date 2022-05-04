1f8373582b4d49874d5683afa282b8deb8acdb2b
Adriel Simé Alburquerque <simealburquerque16@gmail.com>
2022-05-04 06:59:46 -0400

    __

--- src/EduTest.UI/src/views/EditStudentView.vue
+++ src/EduTest.UI/src/views/EditStudentView.vue
@@ -1,9 +1,42 @@
 <template>
     <div>
         <HeaderComponentVue />
-        <div class="container">
-            <div class="form-group">
-
+        <div class="w-50 mx-auto m-5 border border-1 p-5">
+            <div class="row mb-4">
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="firstname">Nombre</label>
+                        <input type="text" id="firstname" class="form-control" />
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="lastname">Apellidos</label>
+                        <input type="text" id="lastname" class="form-control" />
+                    </div>
+                </div>
+            </div>
+            <div class="form-outline mb-4">
+                <label class="form-label" for="dateOfBirth">Nacimiento</label>
+                <input type="date" id="dateOfBirth" class="form-control" />
+            </div>
+            <div class="form-outline mb-4">
+                <label class="form-label" for="email">Correo</label>
+                <input type="email" id="email" class="form-control" />
+            </div>
+            <div class="row mb-4">
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="firstname">Curso</label>
+                        <select name="courses" id="" class="custom-select"></select>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-outline">
+                        <label class="form-label" for="lastname">Materia</label>
+                        <select name="matters" id="" class="custom-select"></select>
+                    </div>
+                </div>
             </div>
         </div>
         <FooterComponentVue />
